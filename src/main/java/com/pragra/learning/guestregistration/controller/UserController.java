4ce3496aa87b9afaae73d23d425a278f8f464716
ShubhamHirpara <shubhamhirpara61@gmail.com>
2021-11-07 16:13:15 -0500

    modified security

--- src/main/java/com/pragra/learning/guestregistration/controller/UserController.java
+++ src/main/java/com/pragra/learning/guestregistration/controller/UserController.java
@@ -4,6 +4,7 @@ import com.pragra.learning.guestregistration.entity.User;
 import com.pragra.learning.guestregistration.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,8 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping
-    public User addUser(User user){
+    @PostMapping()
+    public User addUser(@RequestBody User user){
         return userService.addUser(user);
     }
 }
